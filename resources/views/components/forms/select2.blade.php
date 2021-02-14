6eafe2ba0487956530bd898452c918221824b164
Jeffrey Li <jeffrey@vectorwyse.com>
2021-02-13 22:43:56 -0500

    style select2

--- resources/views/components/forms/select2.blade.php
+++ resources/views/components/forms/select2.blade.php
@@ -1,9 +1,10 @@
 {{-- requires jquery + select2 --}}
+{{-- styles are in resources/css/app.css --}}
 <div wire:ignore
   x-data="{ selected: $wire.entangle('{{ $attributes->wire('model')->value() }}') }" x-init="() => {
     select2 = $($refs.select2).select2();
     select2.on('select2:select', (e) => selected = e.target.value);
     $watch('selected', (value) => select2.val(value).trigger('change'));
 }">
-  <x-forms.select x-ref="select2" x-model="selected" :options="$options" />
+  <x-forms.select x-ref="select2" x-model="selected" :options="$options" style="width:100%" />
 </div>
